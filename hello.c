320d38d86630241765aef02d69812a182d55d321
LikensonD <delsa.likenson@courrier.uqam.ca>
2020-09-28 19:32:59 -0400

    Ajout methode boucleAvant()

--- hello.c
+++ hello.c
@@ -1,6 +1,28 @@
+
 #include <stdio.h>
 
+//Prototypes (signatures) des fonctions
+void boucleAvant();
+
 int main(int argc, char**argv) {
-	printf("Hello, %s \n",argv[1]);
+
+	if (argc > 2) 
+	{
+		printf("Un seul argument permis\n");
+	} else 
+	{
+		printf("Hello, %s \n",argv[1]);
+	}	
+	
+	boucleAvant();
 	return 0;
+}
+
+//Fonction  qui fait un for avec un printf de i
+void boucleAvant()
+{
+	for(int i=0;i<10;++i)
+	{
+		printf("Valeur de i : %i\n",i);
+	}
 }
